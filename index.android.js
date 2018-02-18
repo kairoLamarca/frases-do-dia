0bd8f8b474ddda3fe9a6683f5a1ec53f3c32fcbb
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-02-18 16:08:06 -0300

    estilos css no app
    estilos css no app

--- index.android.js
+++ index.android.js
@@ -5,7 +5,9 @@ import { Text, AppRegistry } from 'react-native';
 //Formatações
 const Estilos = {
   estiloTexto: {
-    fontSize: 50
+    fontSize: 30,
+    backgroundColor: '#08509B',
+    height: 80
   }
 };
 
