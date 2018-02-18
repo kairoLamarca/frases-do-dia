f8de3e16d75bef9a47b094ebefdf8e7008199190
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-02-18 18:22:11 -0300

    utilização de flexbox
    utilização de flexbox

--- index.android.js
+++ index.android.js
@@ -1,30 +1,41 @@
 //Import
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { Text, AppRegistry, View } from 'react-native';
 
 //Formatações
 const Estilos = {
   estiloTexto: {
     fontSize: 40,
     backgroundColor: '#08509B',
-    paddingTop: 15,
-    color: '#fff',
+    //paddingTop: 15,
+    //color: '#fff',
     //fontStyle: 'italic',
     //fontWeight: '100'//100 e 900
     //textAlign: 'center'
-    textDecorationLine: 'underline line-through'
+    //textDecorationLine: 'underline line-through'
     //shadowColor: '#000',
     //shadowOffset: { width: 0, height: 15 },
     //shadowOpacity: 1
     //shadowRadius: 2
     //elevation: 30
+    height: 60,
+    width: 60
+  },
+  estiloView: {
+    backgroundColor: 'skyblue',
+    height: 300,
+    justifyContent: 'center',//alinhar verticalmente
+    alignItems: 'flex-end'//alinhar horizontalmente
   }
 };
 
 //Criar o componente
 const App = () => {
+  const { estiloTexto, estiloView } = Estilos;
   return (
-    <Text style={Estilos.estiloTexto}>Frases do dia</Text>
+    <View style={estiloView}>
+      <Text style={estiloTexto}></Text>
+    </View>
   );
 };
 
