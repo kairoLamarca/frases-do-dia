ea70fd70e975605681ab20bf3b0d6bd575b9a9cf
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-02-18 17:26:04 -0300

    estilos CSS
    estilos CSS

--- index.android.js
+++ index.android.js
@@ -5,14 +5,19 @@ import { Text, AppRegistry } from 'react-native';
 //Formatações
 const Estilos = {
   estiloTexto: {
-    fontSize: 20,
+    fontSize: 40,
     backgroundColor: '#08509B',
     paddingTop: 15,
+    color: '#fff',
+    //fontStyle: 'italic',
+    //fontWeight: '100'//100 e 900
+    //textAlign: 'center'
+    textDecorationLine: 'underline line-through'
     //shadowColor: '#000',
     //shadowOffset: { width: 0, height: 15 },
     //shadowOpacity: 1
     //shadowRadius: 2
-    elevation: 30
+    //elevation: 30
   }
 };
 
