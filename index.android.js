13dbe4c1e2d45d98aa60798b780b82fa23f4a88f
kairoLamarca <lamarca.kairo@gmail.com>
2018-02-19 13:41:15 -0300

    proporções em flexbox
    proporções em flexbox

--- index.android.js
+++ index.android.js
@@ -4,46 +4,66 @@ import { Text, AppRegistry, View } from 'react-native';
 
 //Formatações
 const Estilos = {
-  estiloTexto: {
-    fontSize: 40,
-    backgroundColor: '#08509B',
-    //paddingTop: 15,
-    //color: '#fff',
-    //fontStyle: 'italic',
-    //fontWeight: '100'//100 e 900
-    //textAlign: 'center'
-    //textDecorationLine: 'underline line-through'
-    //shadowColor: '#000',
-    //shadowOffset: { width: 0, height: 15 },
-    //shadowOpacity: 1
-    //shadowRadius: 2
-    //elevation: 30
-    height: 60,
-    width: 60
+  // estiloTexto: {
+  //   fontSize: 40,
+  //   backgroundColor: '#08509B',
+  //   //paddingTop: 15,
+  //   //color: '#fff',
+  //   //fontStyle: 'italic',
+  //   //fontWeight: '100'//100 e 900
+  //   //textAlign: 'center'
+  //   //textDecorationLine: 'underline line-through'
+  //   //shadowColor: '#000',
+  //   //shadowOffset: { width: 0, height: 15 },
+  //   //shadowOpacity: 1
+  //   //shadowRadius: 2
+  //   //elevation: 30
+  //   height: 60,
+  //   width: 60
+  // },
+  // estiloTexto2: {
+  //   fontSize: 40,
+  //   backgroundColor: '#2A48FA',
+  //   height: 60,
+  //   width: 60
+  // },
+  // estiloView: {
+  //   backgroundColor: 'skyblue',
+  //   height: 300,
+  //   justifyContent: 'space-around',//alinhar verticalmente -- center, flex-start, flex-end, space-around, space-between
+  //   alignItems: 'center',//alinhar horizontalmente
+  //   flexDirection: 'column'//orientação em colunas ou linhas
+  // }
+
+  principal: {
+    flex: 1, //1:1 ocupa todo o espaço
+    backgroundColor: 'cornflowerblue'
   },
-  estiloTexto2: {
-    fontSize: 40,
-    backgroundColor: '#2A48FA',
-    height: 60,
-    width: 60
+  topo: {
+    flex: 2,//2:11
+    backgroundColor: 'brown'
   },
-  estiloView: {
-    backgroundColor: 'skyblue',
-    height: 300,
-    justifyContent: 'space-around',//alinhar verticalmente -- center, flex-start, flex-end, space-around, space-between
-    alignItems: 'center',//alinhar horizontalmente
-    flexDirection: 'column'//orientação em colunas ou linhas
+  conteudo: {
+    flex: 8,//8:11
+    backgroundColor: 'yellowgreen'
+  },
+  rodape: {
+    flex: 1,//1:11
+    backgroundColor: 'orangered'
   }
 };
 
 //Criar o componente
 const App = () => {
-  const { estiloTexto, estiloTexto2, estiloView } = Estilos;
+  
+  //const { estiloTexto, estiloTexto2, estiloView } = Estilos;
+  const { principal, topo, conteudo, rodape } = Estilos;
+
   return (
-    <View style={estiloView}>
-      <Text style={estiloTexto}>A</Text>
-      <Text style={estiloTexto2}>B</Text>
-      <Text style={estiloTexto}>C</Text>
+    <View style={principal}>
+      <Text style={topo}>Topo</Text>
+      <Text style={conteudo}>Conteudo</Text>
+      <Text style={rodape}>Rodapé</Text>
     </View>
   );
 };
