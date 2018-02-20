ca8f88eecf9a0e7029ab88e2ab1771922bd0ce65
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-02-19 22:03:51 -0300

    Gerar frase randomica ao clicar em nova frase
    Gerar frase randomica ao clicar em nova frase, utilizando o ramdom e o componente Alert

--- index.android.js
+++ index.android.js
@@ -1,6 +1,6 @@
 //Import
 import React from 'react';
-import { Text, AppRegistry, View, Image, TouchableOpacity } from 'react-native';
+import { Text, AppRegistry, View, Image, TouchableOpacity, Alert } from 'react-native';
 
 //Formatações
 const Estilos = {
@@ -23,7 +23,22 @@ const Estilos = {
   }
 };
 
-const botaoPressionado = () => {
+const gerarNovaFrase = () => {
+
+  var numeroAleatorio = Math.random();
+  numeroAleatorio = Math.floor(numeroAleatorio * 5);
+
+  //frases
+  var frases = Array();
+  frases[0] = 'Que eu não perca o equilíbrio, mesmo sabendo que inúmeras forças querem que eu caia.';
+  frases[1] = 'Em tempos de crise, sábios constroem pontes, enquanto tolos preferem barreiras.';
+  frases[2] = 'Um brinde aos nossos defeitos. Porque com as nossas qualidades ninguém se importa mesmo.';
+  frases[3] = 'Falta de coragem causa perda de momentos incríveis. Arrisque sempre!';
+  frases[4] = 'Que sejamos capazes de enxergar algo de bom em cada momento ruim que nos acontecer.';
+
+  var fraseEscolhida = frases[numeroAleatorio];
+
+  Alert.alert('', fraseEscolhida);
 
 }
 
@@ -35,7 +50,9 @@ const App = () => {
   return (
     <View style={principal}>
       <Image source={require('./imgs/logo.png')} />
-      <TouchableOpacity style={botao}>
+      <TouchableOpacity
+        onPress={gerarNovaFrase}
+        style={botao}>
         <Text style={textoBotao}>Nova frase</Text>
       </TouchableOpacity>
     </View>
