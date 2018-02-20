2a872c922102a324f7614c4a0fc5912f33779686
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-02-19 21:50:31 -0300

    layout inicial do app frases do dia
    layout inicial do app frases do dia

--- index.android.js
+++ index.android.js
@@ -1,121 +1,43 @@
 //Import
 import React from 'react';
-//import { Text, AppRegistry, View, Button } from 'react-native';
-//import { Text, AppRegistry, View, TouchableOpacity } from 'react-native';
-import { Text, AppRegistry, View, Image } from 'react-native';
+import { Text, AppRegistry, View, Image, TouchableOpacity } from 'react-native';
 
 //Formatações
 const Estilos = {
-  // estiloTexto: {
-  //   fontSize: 40,
-  //   backgroundColor: '#08509B',
-  //   //paddingTop: 15,
-  //   //color: '#fff',
-  //   //fontStyle: 'italic',
-  //   //fontWeight: '100'//100 e 900
-  //   //textAlign: 'center'
-  //   //textDecorationLine: 'underline line-through'
-  //   //shadowColor: '#000',
-  //   //shadowOffset: { width: 0, height: 15 },
-  //   //shadowOpacity: 1
-  //   //shadowRadius: 2
-  //   //elevation: 30
-  //   height: 60,
-  //   width: 60
-  // },
-  // estiloTexto2: {
-  //   fontSize: 40,
-  //   backgroundColor: '#2A48FA',
-  //   height: 60,
-  //   width: 60
-  // },
-  // estiloView: {
-  //   backgroundColor: 'skyblue',
-  //   height: 300,
-  //   justifyContent: 'space-around',//alinhar verticalmente -- center, flex-start, flex-end, space-around, space-between
-  //   alignItems: 'center',//alinhar horizontalmente
-  //   flexDirection: 'column'//orientação em colunas ou linhas
-  // }
-
-  // principal: {
-  //   flex: 1, //1:1 ocupa todo o espaço
-  //   backgroundColor: 'cornflowerblue'
-  // },
-  // topo: {
-  //   flex: 2,//2:11
-  //   backgroundColor: 'brown'
-  // },
-  // conteudo: {
-  //   flex: 8,//8:11
-  //   backgroundColor: 'yellowgreen'
-  // },
-  // rodape: {
-  //   flex: 1,//1:11
-  //   backgroundColor: 'orangered'
-  // }
 
   principal: {
-    paddingTop: 40
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  // botao: {
-  //   backgroundColor: '#48BBEC',
-  //   padding: 10,
-  //   borderColor: '#1D8EB8',
-  //   borderWidth: 1,
-  //   borderRadius: 8,
-  //   elevation: 8
-  // },
-  // textoBotao: {
-  //   color: '#fff',
-  //   fontSize: 16,
-  //   fontWeight: 'bold',
-  //   alignSelf: 'center'
-  // }
-  imagem: {
-    justifyContent: 'flex-start',
-    padding: 5
+  botao: {
+    backgroundColor: '#538530',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    marginTop: 20
+  },
+  textoBotao: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold'
   }
-
-
 };
 
-// const botaoPressionado = () => {
-//   alert('Botão pressionado');
-// }
+const botaoPressionado = () => {
+
+}
 
 //Criar o componente
 const App = () => {
 
-  //const { estiloTexto, estiloTexto2, estiloView } = Estilos;
-  //const { principal, topo, conteudo, rodape } = Estilos;
-  //const { principal, botao, textoBotao } = Estilos;
-  const { principal, imagem } = Estilos;
+  const { principal, botao, textoBotao } = Estilos;
 
   return (
-    // <View style={principal}>
-    //   <Text style={topo}>Topo</Text>
-    //   <Text style={conteudo}>Conteudo</Text>
-    //   <Text style={rodape}>Rodapé</Text>
-    // </View>
-
     <View style={principal}>
-      {/* <Button
-        onPress={botaoPressionado}
-        title="Clique aqui"
-        color="#841584"
-        accessibilityLabel="Clique para abrir as notícias!"
-      /> */}
-
-      {/* <TouchableOpacity style={botao}>
-        <Text style={textoBotao}>Clique aqui</Text>
-      </TouchableOpacity> */}
-
-      <Image style={imagem} source={require('./imgs/uvas.png')}>
-        <Text>Legenda para a foto</Text>
-      </Image>
-      {/* <Image source={{ uri: 'http://www.drsamrobbins.com/wp-content/uploads/2014/03/apples-burn-fat-400x400.jpg' }}
-        style={{ width: 400, height: 400 }}/> */}
-
+      <Image source={require('./imgs/logo.png')} />
+      <TouchableOpacity style={botao}>
+        <Text style={textoBotao}>Nova frase</Text>
+      </TouchableOpacity>
     </View>
   );
 };
